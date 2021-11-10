a8ac8e0d0952db764856aa9fad11c36793085b91
MAHUGU <joseymahugu@gmail.com>
2021-11-10 22:23:28 +0300

    Update db.php

--- site/stripe/config/db.php
+++ site/stripe/config/db.php
@@ -1,6 +1,6 @@
 <?php
   // DB Params
-  define("DB_HOST", "localhost");
-  define("DB_USER", "root");
-  define("DB_PASS", "");
-  define("DB_NAME", "paypage");
+  define("DB_HOST", "remotemysql.com");
+  define("DB_USER", "Q2XBABNsQd");
+  define("DB_PASS", "4LJ8nyUoHv");
+  define("DB_NAME", "Q2XBABNsQd");
